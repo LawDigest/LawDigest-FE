e941f2da152e426ce0c9f0fb0e6adac5d9b7f2ac
ijaesin <ijaesinii@gmail.com>
2023-12-04 22:58:48 +0900

    feat: 최근 법안 발의한 정당 카드 퍼블리싱

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,7 +1,14 @@
+import CardItem from './components/CardItem';
+import PartyContainer from './components/party/PartyContainer';
+
 export default function Statistics() {
   return (
     <div className="w-full">
       <h1 className="m-5 text-3xl font-bold ">통계</h1>
+
+      <CardItem title="최근 법안 발의한 정당">
+        <PartyContainer />
+      </CardItem>
     </div>
   );
 }
