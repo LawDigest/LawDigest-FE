c449bc60776d05ba6b7e8d876ad6b396e3ecee88
Collection50 <fkaus65@naver.com>
2023-12-05 15:57:45 +0900

    feat: 랜딩 페이지 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,17 @@
+import SearchBar from '@/components/common/SearchBar';
+import Feed from '@/components/Bill/Feed';
+import BillNav from './components';
+
 function Home() {
   return (
-    <section className="w-full h-full flex flex-col items-center justify-center gap-4 py-8 md:py-10 ">valu</section>
+    <section className="w-full h-full flex flex-col items-center justify-center gap-2 py-[10px] ">
+      <SearchBar />
+      <BillNav />
+      <section className="w-full h-full flex-col">
+        <Feed />
+        <div className="w-full h-[8px] bg-[#E2E8F0] " />
+      </section>
+    </section>
   );
 }
 
