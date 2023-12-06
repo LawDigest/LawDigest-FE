ff23f545d5d7c91e36b80e8009427d1956f7dc7c
Collection50 <fkaus65@naver.com>
2023-12-06 17:35:52 +0900

    feat: 무한 스크롤을 구현하기 위한 useFetchBill hook 구현

---
+++ app/components/Feed/hooks/useFetchBills.ts
@@ -0,0 +1,21 @@
+'use client';
+
+import { useInfiniteQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { PaginationResponse, Bill } from '../types';
+
+const billKeys = {
+  all: ['bills'] as const,
+  lists: () => [...billKeys.all, 'list'] as const,
+};
+
+export const useFetchBills = () =>
+  useInfiniteQuery({
+    queryKey: billKeys.lists(),
+    queryFn: ({ pageParam }: { pageParam: number }) =>
+      axios.get<PaginationResponse<Bill>>('/bills', {
+        params: { page: pageParam },
+      }),
+    initialPageParam: 1,
+    getNextPageParam: ({ data: { isLastPage, pageNumber } }) => (isLastPage ? undefined : pageNumber + 1),
+  });
