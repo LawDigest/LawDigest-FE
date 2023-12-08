b6efa1153b9f09f49b12ed8dd36e1b7caa03e009
Collection50 <fkaus65@naver.com>
2023-12-08 15:55:03 +0900

    feat: 공통으로 사용되는 심사 진행 단계 상수 추가

---
+++ constants/bill.ts
@@ -0,0 +1,8 @@
+export const BILL_STAGE = {
+  reception: 'reception',
+  committeeJudge: 'committeeJudge',
+  districtJudge: 'districtJudge',
+  review: 'review',
+  transfer: 'transfer',
+  promulgation: 'promulgation',
+} as const;
