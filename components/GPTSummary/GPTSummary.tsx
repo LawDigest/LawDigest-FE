db707634cbbae8b9a1b4c053ecc1520c09883541
Collection50 <fkaus65@naver.com>
2023-12-09 18:09:32 +0900

    style: GPT 요약 내용을 알리는 컴포넌트 리팩토링

--- components/GPTSummary/GPTSummary.tsx
+++ components/GPTSummary/GPTSummary.tsx
@@ -4,11 +4,11 @@ export default function GPTSummary() {
   return (
     <section className="w-full h-[30px] relative my-[30px]">
       <div className="flex w-full h-[30px] justify-center items-center gap-[11px]">
-        <div className="w-[25px] h-[1px] bg-black" />
+        <div className="w-[50px] h-[1px] bg-black" />
         <div className="text-base font-normal text-black">Summarized by</div>
         <Image src="/images/gpt.png" alt="gpt-logo" width={25} height={25} />
         <div className="text-base font-normal text-black">ChatGPT 4.0</div>
-        <div className="w-[25px] h-[1px] bg-black" />
+        <div className="w-[50px] h-[1px] bg-black" />
       </div>
     </section>
   );
