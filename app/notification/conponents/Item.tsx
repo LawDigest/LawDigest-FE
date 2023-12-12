ffee49bd147235aa2a4a9ac7b8affddaf120c14f
ijaesin <ijaesinii@gmail.com>
2023-12-12 15:45:05 +0900

    남은 시간을 나타내는 timeRemaining 함수 개발

--- app/notification/conponents/Item.tsx
+++ app/notification/conponents/Item.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image';
+import timeRemaining from '@/lib/timeRemaining';
 
 interface ItemProps {
   logo: string;
   name: string;
   law: string;
   members_number: number;
+  date: string;
 }
 
-export default function Item({ logo, name, law, members_number }: ItemProps) {
+export default function Item({ logo, name, law, members_number, date }: ItemProps) {
   return (
     <div className="flex justify-between">
       <Image src={logo} width={50} height={50} alt="정당 로고 이미지" />
@@ -16,7 +18,7 @@ export default function Item({ logo, name, law, members_number }: ItemProps) {
         <p className="text-[#A1A1AA] text-xs">
           {name} 외 {members_number}인이 &apos;{law}&apos; 을 발의했어요
         </p>
-        <p className="text-[#A1A1AA] text-xs">30분 전</p>
+        <p className="text-[#A1A1AA] text-xs">{timeRemaining(date)}</p>
       </div>
       <button type="button">
         <Image src="images/arrow_right.svg" width={24} height={24} alt="들어가기 이미지" />
