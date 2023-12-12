651562763311b5f4999435cedffe85fd33e7f3f5
Collection50 <fkaus65@naver.com>
2023-12-12 23:01:11 +0900

    feat: Tanstack query 프로바이더 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
-'use client';
-
 import SearchBar from '@/components/common/SearchBar';
 import Feed from './components/Feed';
 import BillNav from './components/BillNav';
-import QueryProvider from './lib/query';
 
 function Home() {
   return (
-    <QueryProvider>
+    <>
       <SearchBar />
       <BillNav />
       <Feed />
-    </QueryProvider>
+    </>
   );
 }
 
