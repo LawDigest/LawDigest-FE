9cd0ab828c7655856d54ab22f976815f5e8ba64d
Collection50 <fkaus65@naver.com>
2023-12-17 18:24:18 +0900

    feat: 의안 타입 추가

---
+++ types/type/congressman.ts
@@ -0,0 +1,13 @@
+export interface Congressman {
+  id: string;
+  name: string;
+  party_name: string;
+  party_image_url: string;
+  elect_sort: string;
+  district: string;
+  commits: string;
+  elected: string;
+  homepage: URL;
+  represent_count: number;
+  public_count: number;
+}
