447565c2e9f89ea49ed1be34e1108d94257da73d
Collection50 <fkaus65@naver.com>
2023-12-18 01:39:46 +0900

    style: 로고 제목에 도현 폰트 적용

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,9 +1,16 @@
 import Link from 'next/link';
+import { Do_Hyeon } from 'next/font/google';
+
+const DO_HYUN = Do_Hyeon({
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 function Header() {
   return (
-    <header className="w-full text-center text-2xl pt-[20px] font-bold">
-      <Link href="/">
+    <header className={`w-full text-center text-2xl pt-[20px] font-bold `}>
+      <Link href="/" className={DO_HYUN.className}>
         모두의 입법
         <hr />
       </Link>
