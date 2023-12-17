67e67a109dc392746b14827b5aa6211730f4d011
Collection50 <fkaus65@naver.com>
2023-12-18 02:23:54 +0900

    refactor: 의안 상세페이지 로직 리팩토링 및 구조 변경

--- app/bill/[id]/apis/type.ts
+++ app/bill/[id]/apis/type.ts
@@ -1,10 +1,14 @@
 import { BILL_STAGE_KO } from '@/constants';
-import { BillResponse } from '@/types';
+import { BillResponse, ValueOf } from '@/types';
 
 export interface BillDetailResponse extends BillResponse {
-  readonly stage: keyof typeof BILL_STAGE_KO;
+  readonly stage: ValueOf<typeof BILL_STAGE_KO>;
   readonly keywords: string[];
   readonly similarBills: { title: string; link: string }[];
   readonly public_proposer_list: string[];
   readonly public_proposer_id_list: string[];
+  readonly proposer_party_count_list: Array<{ name: string; count: number }>;
+  readonly proposer_party_id_list: Array<{ name: string; uuid: string }>;
+  readonly like: number;
+  readonly view: number;
 }
