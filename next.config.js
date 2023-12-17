f2336ef76dae71515b3fb06d50a48c8c030d0472
Collection50 <fkaus65@naver.com>
2023-12-17 18:26:03 +0900

    chore: 외부 이미지를 불러오기 위한 next.config 수정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: process.env.NEXT_PUBLIC_URL,
+        port: '',
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
