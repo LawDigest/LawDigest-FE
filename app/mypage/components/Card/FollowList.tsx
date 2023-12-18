e2ff62b9fd2eb5c620403909d55bfadd825a963a
ijaesin <ijaesinii@gmail.com>
2023-12-18 17:43:03 +0900

    refactor: MyPage 컴포넌트 리팩토링

--- app/mypage/components/Card/FollowList.tsx
+++
@@ -1,7 +0,0 @@
-interface FollowListProps {
-  children: React.ReactNode;
-}
-
-export default function FollowList({ children }: FollowListProps) {
-  return <div className="flex justify-center gap-4">{children}</div>;
-}
--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -1,7 +1,7 @@
 import { RxAvatar } from 'react-icons/rx';
-import CardContainer from './components/Card/CardContainer';
-import FollowItem from './components/Card/FollowItem';
-import FollowList from './components/Card/FollowList';
+import { Card, CardBody, CardHeader, CardFooter } from '@nextui-org/card';
+import { Button } from '@nextui-org/button';
+import FollowItem from './components/Card';
 
 const followList = [
   {
@@ -21,15 +21,22 @@ const followList = [
 export default function MyPage() {
   return (
     <div>
-      <CardContainer>
-        <RxAvatar className="mx-auto mt-2 mb-4 text-black rounded-full bg-gray-1 h-28 w-28" />
+      <Card className="text-white bg-gray-4 w-[394px] h-[270px] rounded-xl mx-auto p-5 mt-5">
+        <CardHeader className="flex justify-between p-0">
+          <h1 className="text-2xl font-bold">내 피드</h1>
+          <Button className="w-20 h-6 text-sm text-white rounded-full bg-primary-3">로그아웃</Button>
+        </CardHeader>
 
-        <FollowList>
+        <CardBody className="p-0">
+          <RxAvatar className="mx-auto text-black rounded-full bg-gray-1 h-28 w-28" />
+        </CardBody>
+
+        <CardFooter className="flex justify-center gap-4">
           {followList.map(({ title, followCount }) => (
             <FollowItem key={title} title={title} followCount={followCount} />
           ))}
-        </FollowList>
-      </CardContainer>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
