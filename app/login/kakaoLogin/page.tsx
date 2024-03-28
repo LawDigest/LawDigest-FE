06614a413fdc69030a97e4e23561bc0e436b9b17
ijaesin <ijaesinii@gmail.com>
2024-03-28 13:43:19 +0900

    feat: 로그인 구현
    로그인 페이지 디자인 변경 (카카오 로그인)
    백엔드 api 연동
    
    Resolves: #46

---
+++ app/login/kakaoLogin/page.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useEffect } from 'react';
+import { setCookie } from 'cookies-next';
+import { Logo } from '@/components/common/Header';
+import { useRouter } from 'next/navigation';
+
+export default function KaKaoLogin() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const accessToken = new URL(window.location.href).searchParams.get('token');
+
+    setCookie('ACCESS_TOKEN', accessToken);
+
+    router.push('/');
+  }, [router]);
+
+  return (
+    <section className="w-[90%] mx-auto h-full flex flex-col justify-center items-center gap-20">
+      <Logo width={222} height={37} />
+
+      <p>로그인 중입니다.</p>
+    </section>
+  );
+}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,34 +1,23 @@
+'use client';
+
 import { Logo } from '@/components/common/Header';
-import { IconEmail, IconPassword } from '@/public/svgs';
-import { Button } from '@nextui-org/button';
-import { Input } from '@nextui-org/input';
-import { Divider } from '@nextui-org/react';
-import Link from 'next/link';
+import { Button, Link as NextUILink } from '@nextui-org/react';
+import Image from 'next/image';
 
 export default function Login() {
   return (
-    <section className="w-[90%] mx-auto h-full flex flex-col justify-center items-center">
+    <section className="w-[90%] mx-auto h-full flex flex-col justify-center items-center gap-20">
       <Logo width={222} height={37} />
-      <form className="w-full h-[300px] mt-[120px]">
-        <div className="flex flex-col gap-4">
-          <Input type="email" variant="underlined" placeholder="이메일 입력" startContent={<IconEmail />} />
-          <Input type="password" variant="underlined" placeholder="비밀번호 입력" startContent={<IconPassword />} />
-        </div>
 
-        <div className="w-full flex flex-col justify-center mt-[100px] gap-3 items-center">
-          <div className="flex items-center gap-3 text-sm">
-            <p>아이디 찾기</p>
-            <Divider orientation="vertical" className="h-4 bg-black" />
-            <p>비밀번호 찾기</p>
-          </div>
-          <Button radius="full" className="w-full h-[58px] text-lg font-medium text-white bg-primary-3">
-            <Link href="/">로그인</Link>
-          </Button>
-          <Button radius="full" className="w-full h-[58px] text-lg font-medium text-white bg-gray-2">
-            <Link href="/signup">회원가입</Link>
-          </Button>
-        </div>
-      </form>
+      <Button
+        as={NextUILink}
+        href={`${process.env.NEXT_PUBLIC_URL}oauth2/authorization/kakao?redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`}
+        variant="bordered"
+        radius="full"
+        startContent={<Image src="/images/kakao.svg" width={22} height={22} alt="카카오톡 로고 이미지" />}
+        className="w-full h-[58px] bg-transparent border-gray-2 border-[1px]">
+        카카오톡으로 시작하기
+      </Button>
     </section>
   );
 }
