f5e6046e829afe1868d73e7c3dbd690a381a06b1
ijaesin <ijaesinii@gmail.com>
2024-04-06 00:45:51 +0900

    feat: 로그아웃/회원탈퇴 오류 수정

--- app/login/layout.tsx
+++ app/login/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import { Layout } from '@/components';
 
 export const metadata: Metadata = {
   title: '회원가입 페이지',
@@ -7,8 +8,10 @@ export const metadata: Metadata = {
 
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
   return (
-    <section className="w-full h-full flex flex-col items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  sm:w-[430px] md:h-[788px]">
-      <section className="w-full h-full overflow-scroll gap-2 py-[10px]">{children}</section>
-    </section>
+    <Layout nav>
+      <section className="w-full h-full flex flex-col items-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  sm:w-[430px] md:h-[788px]">
+        <section className="w-full h-full overflow-scroll gap-2 py-[10px]">{children}</section>
+      </section>
+    </Layout>
   );
 }
--- app/mypage/components/UserInfo/LogoutButton.tsx
+++ app/mypage/components/UserInfo/LogoutButton.tsx
@@ -1,10 +1,21 @@
+import { deleteCookie } from 'cookies-next';
+import { useRouter } from 'next/navigation';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '@/constants';
 import { useCallback } from 'react';
 import { Button } from '@nextui-org/react';
 import { postLogout } from '../../apis';
 
 export default function LogoutButton() {
-  const onClickLogout = useCallback(() => {
-    postLogout();
+  const router = useRouter();
+
+  const onClickLogout = useCallback(async () => {
+    const res = await postLogout();
+
+    if (Number(res.code) === 200) {
+      router.push('/login');
+      deleteCookie(ACCESS_TOKEN);
+      deleteCookie(REFRESH_TOKEN);
+    }
   }, []);
 
   return (
