ba9717e6bb323d4ab0a65460e2f23a9863d7669e
ijaesin <ijaesinii@gmail.com>
2024-04-09 17:17:18 +0900

    feat: 후보자 상세 조회 api response 타입 정의
    Related to: #69

--- types/type/election/district.ts
+++ types/type/election/district.ts
@@ -26,3 +26,17 @@ export interface DistrictCandidateResponse {
   district_candidate_list_dto_list: DistrictCandidateProps[];
   pagination_response: Pagination;
 }
+
+export interface CandidateDetailResponse {
+  name: string;
+  city_name: string;
+  district_name: string;
+  party_image_url: string;
+  career1: string;
+  career2: string;
+  gender: string;
+  age: string;
+  edu: string;
+  district_candidate_image: string;
+  district_candidate_id: string;
+}
