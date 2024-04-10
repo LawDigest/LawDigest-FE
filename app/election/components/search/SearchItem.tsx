70fad3af32716ea7fdbcded617a3bbce15b6bce9
ijaesin <ijaesinii@gmail.com>
2024-04-10 18:48:12 +0900

    feat: 후보자 검색 이미지 예외처리
    candidate_image_url === null 일 경우 사용할 ui 구현
    SearchCandidateProps 타입 수정
    
    Resolves: #84

--- app/election/components/search/SearchItem.tsx
+++ app/election/components/search/SearchItem.tsx
@@ -1,5 +1,5 @@
 import { SearcHCandidateProps } from '@/types';
-// import { Avatar } from '@nextui-org/avatar';
+import { Avatar } from '@nextui-org/avatar';
 import Link from 'next/link';
 import { EnterButton } from '@/components';
 import Image from 'next/image';
@@ -12,6 +12,7 @@ export default function SearchItem({
   name,
   party_name,
   is_district,
+  candidate_image_url,
 }: SearcHCandidateProps) {
   const linkUrl = is_district
     ? `/election/candidate/${district_candidate_id}?type=district`
@@ -20,11 +21,14 @@ export default function SearchItem({
   return (
     <div className="w-full h-[66px] p-0 flex items-center justify-between">
       <div className="flex items-center gap-6">
-        <Image src="/images/basicAvatar.png" width={66} height={66} alt="후보자 더미 데이터" />
-        {/* <Avatar
-          src={process.env.NEXT_PUBLIC_IMAGE_URL + candidate_image_url}
-          className="w-[66px] h-[66px] object-cover border"
-        /> */}
+        {candidate_image_url === null ? (
+          <Image src="/images/basicAvatar.png" width={66} height={66} alt="후보자 더미 데이터" />
+        ) : (
+          <Avatar
+            src={process.env.NEXT_PUBLIC_IMAGE_URL + candidate_image_url}
+            className="w-[66px] h-[66px] object-cover border"
+          />
+        )}
 
         <div className="flex flex-col">
           <p className="text-lg font-semibold">{name}</p>
