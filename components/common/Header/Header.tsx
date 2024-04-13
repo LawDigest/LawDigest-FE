c4a51b95f0d786d3799a2e739ad6ebd682ec8fb2
ijaesin <ijaesinii@gmail.com>
2024-04-14 00:14:30 +0900

    feat: 검색 버튼 누를 시 검색창 토글 구현
    Related to: #87

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -2,11 +2,13 @@
 
 import Link from 'next/link';
 import { Navbar, NavbarContent, NavbarItem, NavbarBrand } from '@nextui-org/navbar';
+import { useCallback, useState } from 'react';
 import GoBackButton from './GoBackButton';
 import SettingButton from './SettingButton';
 import SearchButton from './SearchButton';
 import NotificationButton from './NotifcationButton';
 import Logo from './Logo';
+import { SearchBar } from '../SearchBar';
 
 interface HeaderProps {
   logo?: boolean;
@@ -18,56 +20,66 @@ interface HeaderProps {
 }
 
 export default function Header({ logo, goBack, title, setting, search, notification }: HeaderProps) {
+  const [toggleSearch, setToggleSearch] = useState(false);
+
+  const onClickSearch = useCallback(() => {
+    setToggleSearch(!toggleSearch);
+  }, [toggleSearch]);
+
   return (
-    <Navbar>
-      {logo && (
-        <NavbarBrand>
-          <Link href="/">
-            <Logo width={106} height={18} />
-          </Link>
-        </NavbarBrand>
-      )}
+    <section className="w-full">
+      <Navbar>
+        {logo && (
+          <NavbarBrand>
+            <Link href="/">
+              <Logo width={106} height={18} />
+            </Link>
+          </NavbarBrand>
+        )}
 
-      {goBack && (
-        <NavbarContent justify="start">
-          <NavbarItem>
-            <GoBackButton />
-          </NavbarItem>
-        </NavbarContent>
-      )}
+        {goBack && (
+          <NavbarContent justify="start">
+            <NavbarItem>
+              <GoBackButton />
+            </NavbarItem>
+          </NavbarContent>
+        )}
 
-      {title && (
-        <NavbarContent justify="center">
-          <NavbarItem className="font-medium">{title}</NavbarItem>
-        </NavbarContent>
-      )}
+        {title && (
+          <NavbarContent justify="center">
+            <NavbarItem className="font-medium">{title}</NavbarItem>
+          </NavbarContent>
+        )}
 
-      {setting && (
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <SettingButton />
-          </NavbarItem>
-        </NavbarContent>
-      )}
+        {setting && (
+          <NavbarContent justify="end">
+            <NavbarItem>
+              <SettingButton />
+            </NavbarItem>
+          </NavbarContent>
+        )}
 
-      {search && (
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <SearchButton />
-          </NavbarItem>
-          <NavbarItem>
-            <NotificationButton />
-          </NavbarItem>
-        </NavbarContent>
-      )}
+        {search && (
+          <NavbarContent justify="end">
+            <NavbarItem>
+              <SearchButton onClick={onClickSearch} />
+            </NavbarItem>
+            <NavbarItem>
+              <NotificationButton />
+            </NavbarItem>
+          </NavbarContent>
+        )}
 
-      {notification && (
-        <NavbarContent justify="end">
-          <NavbarItem>
-            <NotificationButton />
-          </NavbarItem>
-        </NavbarContent>
-      )}
-    </Navbar>
+        {notification && (
+          <NavbarContent justify="end">
+            <NavbarItem>
+              <NotificationButton />
+            </NavbarItem>
+          </NavbarContent>
+        )}
+      </Navbar>
+
+      <div className="mb-5">{toggleSearch && <SearchBar isElection={false} />}</div>
+    </section>
   );
 }
