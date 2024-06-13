3b417acb7d04ed2d4c38602879d41effb40b5a3a
ijaesin <ijaesinii@gmail.com>
2024-06-13 20:56:57 +0900

    fix: 바텀네비 css 속성 수정
    fixed => sticky
    
    Related to: #123

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -25,7 +25,7 @@ export default function Layout({
   children,
 }: LayoutProps) {
   return (
-    <section className="w-full h-full flex flex-col items-center sm:w-[430px] lg:w-full min-w-[360px]">
+    <section className="w-full h-full flex flex-col absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 items-center sm:w-[430px] lg:w-full min-w-[360px]">
       <Header
         logo={logo}
         goBack={goBack}
@@ -36,7 +36,11 @@ export default function Layout({
         theme={theme}
       />
       <section className="w-full h-full overflow-scroll max-w-[1280px]">{children}</section>
-      {nav && <Nav />}
+      {nav && (
+        <section className="w-full">
+          <Nav />
+        </section>
+      )}
     </section>
   );
 }
