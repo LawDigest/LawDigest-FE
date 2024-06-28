cbdc982dc1a17bbed1e0fe2bc120074fd17a166b
ijaesin <ijaesinii@gmail.com>
2024-06-28 15:38:19 +0900

    feat: 알림 페이지에 바텀 네비게이션 추가

--- app/notification/layout.tsx
+++ app/notification/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function NotificationLayout({ children }: { children: React.ReactNode }) {
   return (
-    <Layout goBack theme>
+    <Layout goBack theme nav>
       {children}
     </Layout>
   );
