2c5726b0b2b80f188239a7cfb4a7132e6fe93078
ijaesin <ijaesinii@gmail.com>
2024-07-15 16:51:37 +0900

    fix: 심사진행단계 소관위 접수 === 위원회 심사로 변경
    Related to: #145

--- app/bill/[id]/page.tsx
+++ app/bill/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { Bill } from '@/components/Bill';
 import getQueryClient from '@/lib/getQueryClient';
 import { SubHeader } from '@/components';
@@ -17,30 +18,32 @@ export default async function BillDetail({ params: { id } }: { params: { id: str
   const billStage = bill.bill_info_dto.bill_stage;
 
   return (
-    <section className="flex flex-col">
-      <SubHeader title="의안 자세히 보기" />
-      <Bill {...bill} detail viewCount={viewCount}>
-        <section className="lg:w-[490px] lg:float-right">
-          <SectionContainer title="발의자 명단">
-            <ProposerList
-              representativeProposerList={representativeProposerList}
-              publicProposerList={publicProposerList}
-            />
-          </SectionContainer>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <section className="flex flex-col">
+        <SubHeader title="의안 자세히 보기" />
+        <Bill {...bill} detail viewCount={viewCount}>
+          <section className="lg:w-[490px] lg:float-right">
+            <SectionContainer title="발의자 명단">
+              <ProposerList
+                representativeProposerList={representativeProposerList}
+                publicProposerList={publicProposerList}
+              />
+            </SectionContainer>
 
-          <Divider className="hidden lg:block h-[1px] w-full border-gray-1 dark:border-dark-l" />
+            <Divider className="hidden lg:block h-[1px] w-full border-gray-1 dark:border-dark-l" />
 
-          <SectionContainer title="심사 진행 단계">
-            <ProgressStage billStage={billStage} />
-          </SectionContainer>
+            <SectionContainer title="심사 진행 단계">
+              <ProgressStage billStage={billStage} />
+            </SectionContainer>
 
-          <Divider className="hidden lg:block h-[1px] w-full border-gray-1 dark:border-dark-l" />
+            <Divider className="hidden lg:block h-[1px] w-full border-gray-1 dark:border-dark-l" />
 
-          <SectionContainer title="다른 개정안 보기">
-            <AnotherBill similarBills={similarBills} />
-          </SectionContainer>
-        </section>
-      </Bill>
-    </section>
+            <SectionContainer title="다른 개정안 보기">
+              <AnotherBill similarBills={similarBills} />
+            </SectionContainer>
+          </section>
+        </Bill>
+      </section>
+    </HydrationBoundary>
   );
 }
