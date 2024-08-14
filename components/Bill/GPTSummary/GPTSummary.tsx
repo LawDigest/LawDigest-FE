298c63e5a440ee42c188d3cb806b14e9b318dbd8
ijaesin <ijaesinii@gmail.com>
2024-08-14 16:50:59 +0900

    feat: 마이페이지 내 사진 테두리 추가
    Related to: #162

--- components/Bill/GPTSummary/GPTSummary.tsx
+++ components/Bill/GPTSummary/GPTSummary.tsx
@@ -4,7 +4,7 @@ export default function GPTSummary() {
   return (
     <div className="flex justify-center items-center gap-[11px] opacity-30 text-xs font-semibold">
       <p>Summarized by</p>
-      <Image src="/images/gpt.png" alt="gpt-logo" width={25} height={25} />
+      <Image src="/images/gpt.png" alt="gpt-logo" width={25} height={25} priority loader={({ src }) => `${src}`} />
       <p>ChatGPT 4.0</p>
     </div>
   );
