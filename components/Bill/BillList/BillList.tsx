6df19300d5affc2405e291ddd4a7b214c45388bb
ijaesin <ijaesinii@gmail.com>
2024-08-15 19:18:12 +0900

    fix: 발의한 법안이 3개 미만인 의원들의 발의 법안 리스트 제대로 나오도록 수정
    useInfiniteQuery구문 마지막 리턴값에서 last_page가 0일 경우 1을 리턴
    
    Related to: #167

--- components/Bill/BillList/BillList.tsx
+++ components/Bill/BillList/BillList.tsx
@@ -19,13 +19,9 @@ export default function BillList({
 }) {
   return (
     <>
-      {bills.length === 0 ? (
-        <p className="flex justify-center my-8 text-sm text-gray-2 dark:text-gray-3">
-          발의한 법안이 존재하지 않습니다.
-        </p>
-      ) : (
-        bills.map((bill, index) => <Bill key={`${bill.bill_info_dto.bill_id + index}`} {...bill} detail={detail} />)
-      )}
+      {bills.map((bill, index) => (
+        <Bill key={`${bill.bill_info_dto.bill_id + index}`} {...bill} detail={detail} />
+      ))}
       {feedType === 'sorted_by_latest' && isFetching && (
         <div className="flex justify-center w-full my-4">
           <Spinner color="default" />
