eeaf618e1b9d75a0a21bdfede661179c6316b587
ijaesin <ijaesinii@gmail.com>
2024-08-16 18:50:39 +0900

    feat: 마이페이지 팔로우한 정당, 의원 테두리 색상 추가
    Related to: #167

--- app/mypage/components/Party/PartyItem.tsx
+++ app/mypage/components/Party/PartyItem.tsx
@@ -5,7 +5,8 @@ import { FollowingPartyType } from '@/types';
 export default function PartyItem({ party_id, party_name, party_image_url }: FollowingPartyType) {
   return (
     <Link href={`/party/${party_id}`}>
-      <div className="w-[140px] h-[102px] rounded-lg mr-[10px] border-[2px] flex justify-center items-center bg-white">
+      <div
+        className={`w-[140px] h-[102px] rounded-lg mr-[10px] border-1.5 flex justify-center items-center bg-white ${party_name}`}>
         <Image
           src={process.env.NEXT_PUBLIC_IMAGE_URL + party_image_url}
           width={100}
