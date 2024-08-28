d00bfb185101aff6e43ca3b655ccd69a5f38f033
ijaesin <ijaesinii@gmail.com>
2024-08-28 15:11:36 +0900

    feat: 검색 페이지 디자인 변경
    Resolves: #179

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -2,20 +2,13 @@
 
 import { useState, useEffect } from 'react';
 import { BillList, SearchBar } from '@/components';
-import { useIntersect, useTabType } from '@/hooks';
-import { SEARCH_TAB_KO } from '@/constants';
+import { useIntersect } from '@/hooks';
+import { Divider } from '@nextui-org/react';
 import { useGetSearchCongressmanParty, useGetSearchBill } from '../apis';
-import { SearchList, SearchTab } from './components';
+import { SearchList } from './components';
 
 export default function SearchResult({ params: { id } }: { params: { id: string } }) {
-  const [searchType, setSearchType] = useTabType<typeof SEARCH_TAB_KO>('법안');
-  const {
-    data: dataCP,
-    hasNextPage: hasNextPageCP,
-    isFetching: isFetchingCP,
-    fetchNextPage: fetchNextPageCP,
-    refetch: refetchCP,
-  } = useGetSearchCongressmanParty(decodeURI(id));
+  const { data: dataCP, refetch: refetchCP } = useGetSearchCongressmanParty(decodeURI(id));
   const {
     data: dataBill,
     hasNextPage: hasNextPageBill,
@@ -23,19 +16,10 @@ export default function SearchResult({ params: { id } }: { params: { id: string 
     fetchNextPage: fetchNextPageBill,
     refetch: refetchBill,
   } = useGetSearchBill(decodeURI(id));
-  const [searchResultsCP, setSearchResultsCP] = useState(
-    dataCP ? dataCP.pages.flatMap(({ data: { search_response: responses } }) => responses) : [],
-  );
+  const [searchResultsCP, setSearchResultsCP] = useState(dataCP ? dataCP.data.search_response : []);
   const [searchResultsBill, setSearchResultsBill] = useState(
     dataBill ? dataBill.pages.flatMap(({ data: { search_response: responses } }) => responses) : [],
   );
-
-  const fetchRefCP = useIntersect(async (entry: any, observer: any) => {
-    observer.unobserve(entry.target);
-    if (hasNextPageCP && !isFetchingCP) {
-      fetchNextPageCP();
-    }
-  });
 
   const fetchRefBill = useIntersect(async (entry: any, observer: any) => {
     observer.unobserve(entry.target);
@@ -46,7 +30,7 @@ export default function SearchResult({ params: { id } }: { params: { id: string 
 
   useEffect(() => {
     if (dataCP) {
-      setSearchResultsCP(() => [...dataCP.pages.flatMap(({ data: { search_response: responses } }) => responses)]);
+      setSearchResultsCP(() => [...dataCP.data.search_response]);
     }
     if (dataBill) {
       setSearchResultsBill(() => [...dataBill.pages.flatMap(({ data: { search_response: responses } }) => responses)]);
@@ -54,29 +38,39 @@ export default function SearchResult({ params: { id } }: { params: { id: string 
   }, [dataCP, dataBill]);
 
   useEffect(() => {
-    refetchBill();
     refetchCP();
+    refetchBill();
   }, [dataCP, dataBill]);
 
   return (
     <>
       <SearchBar />
       <section className="lg:w-[840px] mx-auto">
-        <p className="my-4 text-sm font-medium text-center text-gray-2">{`'${decodeURI(id)}'에 대한 검색결과입니다.`}</p>
+        <p className="my-4 text-sm font-medium text-center text-gray-2">{`'${decodeURI(id)}'에 대한 검색 결과입니다.`}</p>
         <div className="mb-10">
-          <SearchTab type={searchType as any} clickHandler={setSearchType as any} />
-          {/* eslint-disable-next-line no-nested-ternary */}
-          {searchType === '법안' ? (
-            searchResultsBill.length ? (
+          <div>
+            <h2 className="mx-5 text-lg font-semibold lg:text-xl">의원 · 정당</h2>
+            <div className="flex justify-center mx-5">
+              <Divider className="my-2 bg-gray-1 dark:bg-dark-l" />
+            </div>
+            {searchResultsCP.length ? (
+              <SearchList searchResults={searchResultsCP} />
+            ) : (
+              <p className="my-8 text-sm text-center lg:text-base text-gray-2">검색 결과가 존재하지 않습니다.</p>
+            )}
+          </div>
+
+          <div>
+            <h2 className="mx-5 text-lg font-semibold lg:text-xl">법안</h2>
+            <div className="flex justify-center mx-5">
+              <Divider className="mx-5 mt-2 bg-gray-1 dark:bg-dark-l" />
+            </div>
+            {searchResultsBill.length ? (
               <BillList bills={searchResultsBill} isFetching={isFetchingBill} fetchRef={fetchRefBill} detail={false} />
             ) : (
-              <p className="my-8 text-sm text-center lg:text-base text-gray-2">검색 결과가 없습니다.</p>
-            )
-          ) : searchResultsCP.length ? (
-            <SearchList searchResults={searchResultsCP} isFetching={isFetchingCP} fetchRef={fetchRefCP} />
-          ) : (
-            <p className="my-8 text-sm text-center lg:text-base text-gray-2">검색 결과가 없습니다.</p>
-          )}
+              <p className="my-8 text-sm text-center lg:text-base text-gray-2">검색 결과가 존재하지 않습니다.</p>
+            )}
+          </div>
         </div>
       </section>
     </>
