e2ea3eb761e94586e7ea76ae58328986de5028f5
ijaesin <ijaesinii@gmail.com>
2024-12-04 14:06:42 +0900

    feat: 법안 처리 결과 리스트 표결 숫자 내림차순으로 변경
    Related to: #217

--- app/bill/[id]/components/ProcessResult/ProcessResult.tsx
+++ app/bill/[id]/components/ProcessResult/ProcessResult.tsx
@@ -42,28 +42,30 @@ export default function ProcessResult({
         </div>
       </CardHeader>
       <CardBody className="flex flex-col gap-3 py-5 pl-0 pr-6 basis-1/2">
-        {party_vote_list.map(({ party_info: { party_id, party_name, party_image_url }, party_approval_count }) => (
-          <div key={party_id} className="flex items-center justify-between">
-            <Link
-              href={`/party/${party_id}`}
-              className={`flex items-center justify-center w-8 h-8 rounded-full shadow-lg shrink-0 border-1.5 ${party_name}`}>
-              {party_name === '무소속' ? (
-                <div className="text-xs font-medium text-black">무소속</div>
-              ) : (
-                <Image
-                  src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${isDark ? party_image_url.replace('wide', 'dark') : party_image_url}`}
-                  width={24}
-                  height={24}
-                  alt={`${party_name} 로고 이미지`}
-                />
-              )}
-            </Link>
-            <Link href={`/party/${party_id}`}>
-              <p className="text-xs font-semibold lg:text-sm text-gray-2 dark:text-gray-1">{party_name}</p>
-            </Link>
-            <p className="text-xs lg:text-sm font-medium w-[32px] lg:w-[40px]">{party_approval_count}표</p>
-          </div>
-        ))}
+        {party_vote_list
+          .sort((a, b) => b.party_approval_count - a.party_approval_count)
+          .map(({ party_info: { party_id, party_name, party_image_url }, party_approval_count }) => (
+            <div key={party_id} className="flex items-center justify-between">
+              <Link
+                href={`/party/${party_id}`}
+                className={`flex items-center justify-center w-8 h-8 rounded-full shadow-lg shrink-0 border-1.5 ${party_name}`}>
+                {party_name === '무소속' ? (
+                  <div className="text-xs font-medium text-black">무소속</div>
+                ) : (
+                  <Image
+                    src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${isDark ? party_image_url.replace('wide', 'dark') : party_image_url}`}
+                    width={24}
+                    height={24}
+                    alt={`${party_name} 로고 이미지`}
+                  />
+                )}
+              </Link>
+              <Link href={`/party/${party_id}`}>
+                <p className="text-xs font-semibold lg:text-sm text-gray-2 dark:text-gray-1">{party_name}</p>
+              </Link>
+              <p className="text-xs lg:text-sm font-medium w-[32px] lg:w-[40px]">{party_approval_count}표</p>
+            </div>
+          ))}
       </CardBody>
     </Card>
   );
