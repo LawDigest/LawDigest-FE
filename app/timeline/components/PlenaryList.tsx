78d1370a4babac3b0a579be3d6abcc2757842f86
ijaesin <ijaesinii@gmail.com>
2025-04-20 13:25:03 +0900

    fix: 타임라인 페이지 본회의 심사 밑 심사 결과가 법안마다 밑에 나오지 않는 버그 수정

--- app/timeline/components/PlenaryList.tsx
+++ app/timeline/components/PlenaryList.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useTheme } from 'next-themes';
-import { Card, CardBody, Chip, Pagination, Button, useDisclosure } from '@nextui-org/react';
+import { Card, CardBody, Chip, Button, useDisclosure, Pagination } from '@nextui-org/react';
 import { ProcessResult } from '@/app/bill/[id]/components';
 import { useState, useEffect } from 'react';
 import { IconEnter, IconNext, IconPrev } from '@/public/svgs';
@@ -24,6 +24,16 @@ export default function PlenaryList({
       bill_stage: string;
       bill_proposers: string;
       bill_brief_summary: string;
+      bill_outline_dto_list: {
+        party_info: {
+          party_id: number;
+          party_name: string;
+          party_image_url: string;
+        }[];
+        bill_id: string;
+        bill_proposers: string;
+        bill_brief_summary: string;
+      }[];
     };
     approval_vote_count: number;
     total_vote_count: number;
@@ -59,11 +69,11 @@ export default function PlenaryList({
   const currentItems = plenary_list.slice(startIndex, endIndex);
 
   return (
-    <div className="flex flex-col gap-5">
+    <section className="flex flex-col gap-5">
       <div className="relative">
         <div className="bg-gray-3 dark:bg-gray-4 w-[10px] h-[10px] rounded-full border border-black absolute -left-[30px] top-4" />
         <div className="flex items-center justify-between">
-          <h3 className="text-[26px] font-extralight">본회의 심의</h3>
+          <h3 className="text-[26px] font-extralight">본회의 심사</h3>
           {plenary_list.length !== 0 && (
             <div className="flex items-center">
               <p className="text-sm font-medium text-gray-2 dark:text-gray-3">
@@ -73,24 +83,33 @@ export default function PlenaryList({
                 <IconEnter />
               </Button>
               <TimelineModal isOpen={isOpen} onClose={onClose}>
-                <div className="flex flex-col gap-3">
-                  {plenary_list.map(({ bill_info: { party_info, bill_id, bill_proposers, bill_brief_summary } }) => (
-                    <div key={bill_id} className="flex gap-[18px] items-center">
-                      <Link
-                        href={`/party/${party_info[0].party_id}`}
-                        className={`flex items-center justify-center w-7 h-7 rounded-full shadow-lg shrink-0 border-1.5 ${party_info[0].party_name}`}>
-                        <Image
-                          src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${isDark ? party_info[0].party_image_url.replace('wide', 'dark') : party_info[0].party_image_url}`}
-                          alt={`${party_info[0].party_name} 로고 이미지`}
-                          width={22}
-                          height={22}
-                        />
-                      </Link>
-                      <div className="flex flex-col gap-1">
-                        <Link href={`/bill/${plenary_list[currentPage].bill_info.bill_id}`}>
-                          <p className="text-xs font-bold">{bill_brief_summary}</p>
-                        </Link>
-                        <p className="text-xs font-semibold text-gray-2 dark:text-gray-3">{bill_proposers}</p>
+                <div className="flex flex-col gap-4">
+                  {plenary_list.map(({ bill_info }) => (
+                    <div key={bill_info.bill_id} className="flex flex-col gap-2">
+                      <p className="text-lg font-bold">{bill_info.bill_name}</p>
+                      <div className="flex flex-col gap-3">
+                        {bill_info.bill_outline_dto_list?.map(
+                          ({ party_info, bill_id, bill_proposers, bill_brief_summary }) => (
+                            <div key={bill_id} className="flex gap-[18px] items-center">
+                              <Link
+                                href={`/party/${party_info[0].party_id}`}
+                                className={`flex items-center justify-center w-7 h-7 rounded-full shadow-lg shrink-0 border-1.5 ${party_info[0].party_name}`}>
+                                <Image
+                                  src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${isDark ? party_info[0].party_image_url.replace('wide', 'dark') : party_info[0].party_image_url}`}
+                                  alt={`${party_info[0].party_name} 로고 이미지`}
+                                  width={22}
+                                  height={22}
+                                />
+                              </Link>
+                              <div className="flex flex-col gap-1">
+                                <Link href={`/bill/${bill_id}`}>
+                                  <p className="text-xs font-bold">{bill_brief_summary}</p>
+                                </Link>
+                                <p className="text-xs font-semibold text-gray-2 dark:text-gray-3">{bill_proposers}</p>
+                              </div>
+                            </div>
+                          ),
+                        )}
                       </div>
                     </div>
                   ))}
@@ -102,13 +121,14 @@ export default function PlenaryList({
       </div>
       <div>
         {plenary_list.length !== 0 ? (
-          <div className="grid grid-cols-1 gap-5 lg:grid-cols-3">
-            {currentItems.map((item) => (
+          <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+            {currentItems.map((item, index) => (
               <Card key={item.bill_info.bill_id} className="z-10 overflow-visible">
                 <CardBody className="py-3 overflow-visible">
                   <Link
                     href={`/party/${item.bill_info.party_info[0].party_id}`}
-                    className={`absolute -left-[39px] flex items-center justify-center w-7 h-7 rounded-full shadow-lg shrink-0 border-1.5 bg-white dark:bg-dark-b ${item.bill_info.party_info[0].party_name} lg:hidden`}>
+                    className={`absolute -left-[39px] flex items-center justify-center w-7 h-7 rounded-full shadow-lg shrink-0 border-1.5 bg-white dark:bg-dark-b ${item.bill_info.party_info[0].party_name}`}
+                    style={index > 0 ? { top: `${index * 50}px` } : {}}>
                     <Image
                       src={`${process.env.NEXT_PUBLIC_IMAGE_URL}${isDark ? item.bill_info.party_info[0].party_image_url.replace('wide', 'dark') : item.bill_info.party_info[0].party_image_url}`}
                       alt={`${item.bill_info.party_info[0].party_name} 로고 이미지`}
@@ -116,7 +136,7 @@ export default function PlenaryList({
                       height={22}
                     />
                   </Link>
-                  <div className="flex flex-col w-full gap-2 lg:h-full lg:justify-between">
+                  <div className="flex flex-col w-full gap-2">
                     <Link href={`/bill/${item.bill_info.bill_id}`}>
                       <p className="text-sm font-bold">{item.bill_info.bill_brief_summary}</p>
                     </Link>
@@ -138,58 +158,52 @@ export default function PlenaryList({
             ))}
           </div>
         ) : (
-          <Card className="z-10 overflow-visible">
-            <CardBody className="py-3 overflow-visible">
-              <p className="text-sm font-bold text-center">심의한 법안이 없습니다.</p>
-            </CardBody>
-          </Card>
-        )}
-        <Card className="w-[calc(100%-20px)] mx-auto -top-[14px] z-[5] lg:hidden">
-          <CardBody />
-        </Card>
-        <Card className="w-[calc(100%-40px)] mx-auto -top-[28px] lg:hidden">
-          <CardBody />
-        </Card>
-        {plenary_list.length !== 0 && (
-          <div className="flex items-center justify-between">
-            <Button
-              isIconOnly
-              size="sm"
-              className="p-0 bg-transparent"
-              onPress={() =>
-                setCurrentPage((prev) => (prev > 0 ? prev - 1 : Math.ceil(plenary_list.length / itemsPerPage) - 1))
-              }>
-              <IconPrev />
-            </Button>
-            <Pagination
-              total={Math.ceil(plenary_list.length / itemsPerPage)}
-              page={currentPage + 1}
-              onChange={(page) => setCurrentPage(page - 1)}
-              classNames={{
-                item: 'bg-gray-1 text-transparent border-none shadow-none w-[6px] h-[6px]',
-                cursor: 'bg-gray-3 text-transparent w-[6px] h-[6px]',
-              }}
-            />
-            <Button
-              isIconOnly
-              size="sm"
-              className="p-0 bg-transparent"
-              onPress={() =>
-                setCurrentPage((prev) => (prev < Math.ceil(plenary_list.length / itemsPerPage) - 1 ? prev + 1 : 0))
-              }>
-              <IconNext />
-            </Button>
-          </div>
+          <p className="text-sm font-bold text-center">심사한 법안이 없습니다.</p>
         )}
       </div>
-
-      {plenary_list.length !== 0 && plenary_list[currentPage].party_vote_list && (
-        <ProcessResult
-          approval_count={plenary_list[currentPage].approval_vote_count}
-          total_vote_count={plenary_list[currentPage].total_vote_count}
-          party_vote_list={plenary_list[currentPage].party_vote_list}
-        />
+      {plenary_list.length !== 0 && (
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-3">
+          {currentItems.map((item) => (
+            <ProcessResult
+              key={item.bill_info.bill_id}
+              approval_count={item.approval_vote_count}
+              total_vote_count={item.total_vote_count}
+              party_vote_list={item.party_vote_list}
+            />
+          ))}
+        </div>
       )}
-    </div>
+      {plenary_list.length !== 0 && (
+        <div className="flex items-center justify-between w-full md:w-[calc(100%-40px)] mx-auto">
+          <Button
+            isIconOnly
+            size="sm"
+            className="p-0 bg-transparent"
+            onPress={() =>
+              setCurrentPage((prev) => (prev > 0 ? prev - 1 : Math.ceil(plenary_list.length / itemsPerPage) - 1))
+            }>
+            <IconPrev />
+          </Button>
+          <Pagination
+            total={Math.ceil(plenary_list.length / itemsPerPage)}
+            page={currentPage + 1}
+            onChange={(page) => setCurrentPage(page - 1)}
+            classNames={{
+              item: 'bg-gray-1 dark:bg-gray-3 text-transparent border-none shadow-none w-[6px] h-[6px]',
+              cursor: 'bg-gray-3 dark:bg-gray-2 text-transparent w-[6px] h-[6px]',
+            }}
+          />
+          <Button
+            isIconOnly
+            size="sm"
+            className="p-0 bg-transparent"
+            onPress={() =>
+              setCurrentPage((prev) => (prev < Math.ceil(plenary_list.length / itemsPerPage) - 1 ? prev + 1 : 0))
+            }>
+            <IconNext />
+          </Button>
+        </div>
+      )}
+    </section>
   );
 }
