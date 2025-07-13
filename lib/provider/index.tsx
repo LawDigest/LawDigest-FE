09d0bc3deac3b5e7575a32c2521f165aaed5099e
ijaesin <ijaesinii@gmail.com>
2025-07-13 15:20:55 +0900

    feat: NextUI 프로바이더 삭제

--- lib/provider/index.tsx
+++ lib/provider/index.tsx
@@ -1,6 +1,5 @@
 import QueryClientProvider from './QueryClientProvider';
-import NextUIProvider from './NextUIProvider';
 import NextThemesProvider from './NextThemesProvider';
 import RecoilRootProvider from './RecoilRootProvider';
 
-export { QueryClientProvider, NextUIProvider, NextThemesProvider, RecoilRootProvider };
+export { QueryClientProvider, NextThemesProvider, RecoilRootProvider };
