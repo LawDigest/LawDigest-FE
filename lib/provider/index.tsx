417cd9bea8bd45b8c2523a5b9268d653d37070f3
ijaesin <ijaesinii@gmail.com>
2025-07-20 17:11:20 +0900

    feat: app 폴더 안에 프로젝트에서 공용으로 사용될 코드를 관리할 common폴더 및 파일 정의

--- lib/provider/index.tsx
+++
@@ -1,5 +0,0 @@
-import QueryClientProvider from './QueryClientProvider';
-import NextThemesProvider from './NextThemesProvider';
-import RecoilRootProvider from './RecoilRootProvider';
-
-export { QueryClientProvider, NextThemesProvider, RecoilRootProvider };
